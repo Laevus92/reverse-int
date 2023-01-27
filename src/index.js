2d1ab830336ca1bdc95b776ed32cef34d72db667
Laevus92 <laevus@yandex.ru>
2023-01-27 17:31:46 +0300

    resolved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    n = Math.abs(n).toString();
+    let result = ''
+    for (let i = (n.length - 1); i >= 0; i--) {
+      result = result + n[i];
+    }
   
+    return Number(result);
 }
